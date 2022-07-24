8b3902e7d4d42be53800d21beef31e8bf1b072cd
Razzak-Musaev <98238271+Razzak-Musaev@users.noreply.github.com>
2022-07-24 15:39:12 +0600

    Update script3.js

---
+++ destrfun/script3.js
@@ -0,0 +1,9 @@
+func(["Иван", "Иванов", "отдел разработки"]);
+
+function func([name, surname, department, position = "джуниор", salary]) {
+	console.log(name);
+	console.log(surname);
+	console.log(department);
+	console.log(position);
+	console.log(salary);
+}
